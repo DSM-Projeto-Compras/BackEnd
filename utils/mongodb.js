87544b773fac44a61aa36ebc77cd024c3a5d3284
Felipe Thiago <felipethiagodasilva@gmail.com>
2025-06-20 22:32:02 -0300

    fix: mongo connection fix

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -19,7 +19,6 @@ if (!MONGODB_DB) {
 
 export const connectToDatabase = async () => {
     try {
-        // Remover barra final, se existir, em MONGODB_URI para evitar duplicidade de barras
         const uri = `${MONGODB_URI.replace(/\/$/, '')}/${MONGODB_DB}`;
         await mongoose.connect(uri, {
             useNewUrlParser: true,
@@ -28,7 +27,7 @@ export const connectToDatabase = async () => {
         console.log('Conectado ao MongoDB');
     } catch (err) {
         console.error('Erro ao conectar ao MongoDB', err);
-        process.exit(1);
+        throw err;
     }
 };
 
